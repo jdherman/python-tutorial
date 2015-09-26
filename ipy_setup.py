88ca7a6d29b9a2f44332714b6b8822df5f296e08
jdherman <jdherman8@gmail.com>
2015-09-26 16:38:12 -0700

    initial commit

---
+++ ipy_setup.py
@@ -0,0 +1,10 @@
+def setup():
+	import numpy as np
+	import matplotlib.pyplot as plt
+	import seaborn as sns
+	import pandas as pd
+	import scipy as sp
+	from IPython.display import set_matplotlib_formats
+	set_matplotlib_formats
+	set_matplotlib_formats('svg')
+
---
+++ test.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[5]:
+
+# get_ipython().magic(u'matplotlib inline')
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats
+# set_matplotlib_formats('svg')
+
+
+# ####Test
+# This is some text
+# This is an equation:
+# $a = \frac{b}{c}$
+
+# In[6]:
+
+v = np.random.rand(100)
+
+
+# In[7]:
+
+plt.plot(v)
+plt.show()
+
+
+# In[ ]:
+
+
+
