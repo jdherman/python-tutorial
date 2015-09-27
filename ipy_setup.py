faef5718a1a26e9c61ead5d807fd0dfab347c320
jdherman <jdherman8@gmail.com>
2015-09-27 14:00:23 -0700

    update

--- ipy_setup.py
+++ ipy_setup.py
@@ -7,4 +7,12 @@ def setup():
 	from IPython.display import set_matplotlib_formats
 	set_matplotlib_formats
 	set_matplotlib_formats('svg')
+  plt.rcParams['figure.figsize'] = (8, 6)
+  plt.rcParams['font.size'] = 13
+  plt.rcParams['font.family'] = 'Source Sans Pro'
+  plt.rcParams['axes.labelsize'] = 1.1*plt.rcParams['font.size']
+  plt.rcParams['axes.titlesize'] = 1.1*plt.rcParams['font.size']
+  plt.rcParams['legend.fontsize'] = plt.rcParams['font.size']
+  plt.rcParams['xtick.labelsize'] = plt.rcParams['font.size']
+  plt.rcParams['ytick.labelsize'] = plt.rcParams['font.size']
 
